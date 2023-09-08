70b910f7b0b840c488b8d43680dd17b7e61e607c
Jin Igarashi <igarashi.jin@water-gis.com>
2023-09-08 20:43:09 +0100

    add footer

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -4,6 +4,20 @@
 	<slot />
 </div>
 
+<footer class="footer">
+	<div class="content has-text-centered">
+		<p>
+			<strong>Static image API powered</strong> by
+			<a href="https://undp.org">United Nations Development Programme</a>. The source code is
+			licensed
+			<a href="https://github.com/UNDP-Data/geohub-static-image-api/blob/main/LICENSE"
+				>BSD-3 License</a
+			>. The source code repository is available at
+			<a href="https://github.com/UNDP-Data/geohub-static-image-api">GitHub</a>.
+		</p>
+	</div>
+</footer>
+
 <style global lang="scss">
 	@import '@undp-data/undp-bulma/bulma.scss';
 </style>

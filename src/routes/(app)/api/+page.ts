511d0e80f8a3ec997763f165ac82c4f4b54810cf
Jin Igarashi <igarashi.jin@water-gis.com>
2023-10-03 17:23:34 +0100

    Feat/add pagemeta (#7)
    * add page meta
    
    * format

--- src/routes/(app)/api/+page.ts
+++ src/routes/(app)/api/+page.ts
@@ -1,8 +1,8 @@
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async () => {
-	const title = 'API Docs | GeoHub';
-	const content = 'GeoHub API Spec';
+	const title = 'API Docs | Static API';
+	const content = 'GeoHub Static API Spec';
 	const spec = '/api/swagger/spec.json';
 
 	return {
